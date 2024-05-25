5ec6844d8431d6bda23ab1ca6049c75af8c34b0f
Patrick Louis <patricklouisw@gmail.com>
2024-05-25 18:31:22 -0400

    Environment setup

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import { Nunito } from "next/font/google";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const font = Nunito({
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Airbnb",
+  description: "Airbnb clone",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={font.className}>{children}</body>
+    </html>
+  );
+}
